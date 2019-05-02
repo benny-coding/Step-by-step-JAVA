cd25e0f7e6ed5e359d7a93196f1e398066c5f267
ohdongpodong <sksms482@naver.com>
2019-05-02 21:53:02 +0900

    spring_mvc setting

--- 2_spring_test/src/main/java/webservice/SampleController.java
+++ 2_spring_test/src/main/java/webservice/SampleController.java
@@ -1,4 +1,13 @@
 package webservice;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+
+    @RequestMapping("/")
+    public String sample () {
+        return "index";
+    }
 }
