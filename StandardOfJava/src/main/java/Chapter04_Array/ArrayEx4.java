e2709a86f654538536eac36dc1d3f1efc80b1a79
ohdongpodong <sksms482@naver.com>
2019-05-24 23:47:24 +0900

    Array3

--- StandardOfJava/src/main/java/Chapter04_Array/ArrayEx4.java
+++ StandardOfJava/src/main/java/Chapter04_Array/ArrayEx4.java
@@ -2,29 +2,23 @@ package Chapter04_Array;
 
 public class ArrayEx4 {
     public static void main(String[] args) {
+        char[] abc = { 'A', 'B', 'C', 'D'};
+        char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        System.out.println(abc);
+        System.out.println(num);
 
-        // 45개의 정수값을 입력하기 위한 배열 생성
-        int[] ball = new int[45];
+        // 배열 abc와 num을 붙여서 하나의 배열(result)로 만든다.
+        char[] result = new char[abc.length+num.length];
+        System.arraycopy(abc, 0, result, 0, abc.length);
+        System.arraycopy(num, 0, result, abc.length, num.length);
+        System.out.println(result); // # A,B,C,D,4,5,6,7,8,9 예상 - 실패
 
-        // 배열의 각 요소에 1~45의 값을 저장한다.
-        for (int i = 0; i < ball.length; i++) {
-            ball[i] = i + 1;
-        }
+        // 배열 abc을 배열 num의 첫 번째 위치부터 배열 abc의 길이만큼 복사
+        System.arraycopy(abc, 0, num, 0, abc.length);
+        System.out.println(num); // # A,B,C,D,4,5,6,7,8,9 예상 - 성공
 
-        int tmp = 0;  // 두 값을 바꾸는데 사용할 임시변수
-        int j = 0;    // 임의의 값 얻어서 저장할 변수
-
-        // 배열에서 저장된 값이 잘 섞이도록 충분히 큰 반복횟수를 지정한다
-        // 배열의 첫번째 요소와 임의의 요소에 저장된 값을 서로 바꿔서 값을 섞는다.
-        for (int i = 0; i < 100; i++) {
-            j = (int) (Math.random() * 45);
-            tmp = ball[0];
-            ball[0] = ball[j];
-            ball[j] = tmp;
-        }
-        for (int i = 0; i < 6; i++) {
-            System.out.print(ball[i] + " ");
-        }
+        // number의 인덱스6 위치에 3개를 복사
+        System.arraycopy(abc,0,num,6,3);
+        System.out.println(num); // # 0,1,2,3,4,5,A,B,C,9 예상 - 실패
     }
 }
-
