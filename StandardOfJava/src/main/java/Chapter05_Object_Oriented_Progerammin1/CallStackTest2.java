c61980f9f0566945f2261a567395f67f640ff149
ohdongpodong <ohdongpodong@gmail.com>
2019-06-01 13:36:34 +0900

    ObjectOrient3_1

---
+++ StandardOfJava/src/main/java/Chapter05_Object_Oriented_Progerammin1/CallStackTest2.java
@@ -0,0 +1,20 @@
+package Chapter05_Object_Oriented_Progerammin1;
+
+public class CallStackTest2 {
+    public static void main(String[] args){
+        System.out.println("main(String[] args)이 시작");
+        firstMethod();
+        System.out.println("main(String[] args)이 끝");
+    }
+
+    static void firstMethod() {
+        System.out.println("firstMethod()이 시작되었음.");
+        secondMethod();
+        System.out.println("firstMethod()이 끝남");
+    }
+
+    static void secondMethod() {
+        System.out.println("secondMethod()이 시작");
+        System.out.println("secondMethod()이 끝남");
+    }
+}
