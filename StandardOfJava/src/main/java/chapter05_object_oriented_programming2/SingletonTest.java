f9d1f0a3c009f477dc9ba924df5ebbaadd3713d6
ohdongpodong <ohdongpodong@gmail.com>
2019-06-11 23:05:24 +0900

    ObjectOriented13

---
+++ StandardOfJava/src/main/java/chapter05_object_oriented_programming2/SingletonTest.java
@@ -0,0 +1,24 @@
+package chapter05_object_oriented_programming2;
+
+final class Singleton {
+    private static Singleton s = new Singleton();
+
+    private Singleton() {
+        //...
+    }
+
+    public static Singleton getInstance() {
+        if(s==null){
+            s = new Singleton();
+        }
+        return s;
+    }
+    // ...
+}
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        // Singleton s = new Singleton(); 생성자가 private로 선언되었기 때문에 인스턴스화가 불가능 하다
+        Singleton s = Singleton.getInstance();
+    }
+}
