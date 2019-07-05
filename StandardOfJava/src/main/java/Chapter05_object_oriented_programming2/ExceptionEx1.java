e1110b7f3ea1917b432546d4c19b3111974303d3
ohdongpodong <ohdongpodong@gmail.com>
2019-07-05 23:11:24 +0900

    exception handling1

---
+++ StandardOfJava/src/main/java/Chapter05_object_oriented_programming2/ExceptionEx1.java
@@ -0,0 +1,16 @@
+package Chapter05_object_oriented_programming2;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try {
+            } catch (Exception e) {
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+        } catch (Exception e) {
+        } // try-catch의 끝
+    } // main메서드의 끝
+}
