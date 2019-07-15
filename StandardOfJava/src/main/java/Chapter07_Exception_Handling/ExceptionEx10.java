f21a2418f98374bc1a11dddac89c4951f5855eb5
ohdongpodong <ohdongpodong@gmail.com>
2019-07-15 21:38:06 +0900

    exception handling8

---
+++ StandardOfJava/src/main/java/Chapter07_Exception_Handling/ExceptionEx10.java
@@ -0,0 +1,7 @@
+package Chapter07_Exception_Handling;
+import java.lang.Exception;
+public class ExceptionEx10 {
+    public static void main(String[] args) {
+        throw new Exception();      // Exception을 고의로 발생시킨다.
+    }
+}
