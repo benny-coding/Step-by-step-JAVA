c07d56f8a437aef5704fe6950cdaeef4a047b3bf
ohdongpodong <ohdongpodong@gmail.com>
2019-08-05 23:15:56 +0900

    ReFactoring1_2

--- CommandTypeBoard/src/main/java/Board/Controller.java
+++ CommandTypeBoard/src/main/java/Board/Controller.java
@@ -1,4 +1,38 @@
 package Board;
 
+import java.util.Scanner;
+
 public class Controller {
+
+    static private String action;
+
+    static View ViewController = new View();
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void untrue_Action(){
+        System.out.print("잘못된 값을 입력하셨습니다. 다시 입력해주세요 : ");
+        action = scanner.nextLine();
+    }
+
+    public static void main(String[] args) {
+
+        ViewController.show_Intro();
+        action = scanner.nextLine();
+
+        while(true) {
+
+            if (action.equals("1")) {
+                ViewController.show_BoardList();
+
+                break;
+            } else if (action.equals("2")) {
+                ViewController.show_End();
+                break;
+            } else {
+                untrue_Action();
+            }
+        }
+    }
+
 }
--- CommandTypeBoard/src/main/java/Board/View.java
+++ CommandTypeBoard/src/main/java/Board/View.java
@@ -1,4 +1,47 @@
 package Board;
 
 public class View {
+
+    protected void show_Intro(){
+
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("                          자바 MVC 커맨드 게시판                            ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("( 시작 : 1 , 종료 : 2 )");
+        System.out.print("입력 : ");
+    }
+
+    protected void show_End(){
+
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("                               게시판 종료                                ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------");
+
+    }
+
+    protected void show_BoardList(){
+
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("| 번호 |              제목              |  글쓴이  |     날짜     |  조회수  |");
+        System.out.println("-----------------------------------------------------------------------");
+        Board.loadBoardList();
+        System.out.println("-----------------------------------------------------------------------");
+
+    }
 }
